ad9bd5feb554f39b02f60d52b6ead905f3f7be36
Kartik Parihar <kartikvips@gmail.com>
2018-04-20 14:31:16 -0700

    finished tree_node

---
+++ W1D5/skeleton/lib/00_tree_node.rb
@@ -0,0 +1,79 @@
+require 'byebug'
+
+class PolyTreeNode
+
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(dad)
+    parent.children.delete(self) if parent && parent != dad
+    @parent = dad
+    dad.children << self unless dad.nil? || parent.children.include?(self)
+
+  end
+
+  def remove_child(child)
+    raise "Is not my child!" unless children.include?(child)
+    child.parent = nil
+  end
+
+  def inspect
+    value
+  end
+
+  def add_child(child)
+    child.parent = self
+  end
+
+  def dfs(target)
+
+    return self if value == target
+
+    self.children.each do |child|
+      rec = child.dfs(target)
+
+      return rec if rec
+    end
+    nil
+
+  end
+
+  def bfs(target)
+
+    node_arr = [self]
+
+    until node_arr.length==0
+      node = node_arr.shift
+      if node.value == target
+        return node
+      end
+      node_arr += node.children
+    end
+    nil
+
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
